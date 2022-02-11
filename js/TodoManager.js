7f0d26fa58cc638eaab09f45b52d6ecb64252a1b
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-11 15:47:42 +0100

    button

--- js/TodoManager.js
+++ js/TodoManager.js
@@ -1,14 +1,26 @@
 
 export class TodoManager {
+
 	constructor(document) {
+
 		this.document = document;
+
 		this.todoManagerElem = this.document.querySelector('.todoManager');
 		this.titleElem = this.document.querySelector('h2');
+		this.buttonElem = this.todoManagerElem.querySelector('button');
+
 		this.title = 'Todo Manager';
+		this.buttonText = 'Add Todo';
+		this.todos = [];
+
 		this.initialize();
 	}
+	
 	initialize() {
 		this.todoManagerElem.style.backgroundColor = 'tan';
 		this.titleElem.innerHTML = this.title;
+		this.buttonElem.innerHTML = this.buttonText;
 	}
+
+
 }
